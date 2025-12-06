296115b15b3e4091f07708a6a16e91127094372d
Sam Clusker <9279784+samclusker@users.noreply.github.com>
2025-12-06 11:51:20 +0000

    terraform ci fix

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,5 +1,4 @@
 # Optional Route53 Hosted Zone for Kubernetes Ingress
-# checkov:skip=CKV2_AWS_39: "Not a requirmement for logging at this time"
 resource "aws_route53_zone" "main" {
   count = var.create_dns_zone ? 1 : 0
 
